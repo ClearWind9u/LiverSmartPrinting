747eb58761613e4c9ce47c85ea42be8138f8279a
nghiavolecra <nghiavo6777@gmail.com>
2024-12-07 15:55:04 +0700

    Thêm giao diện login ví và avatar thông tin cá nhân

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { loginSuccess } from "../redux/userSlice";
 import { useDispatch } from "react-redux";
+import { loginSuccess } from "../redux/userSlice";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
@@ -12,7 +12,6 @@ const LoginForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // Gọi API login
       const response = await axios.post("http://localhost:5000/login", {
         email,
         password,
@@ -23,72 +22,82 @@ const LoginForm = () => {
       if (data.success) {
         dispatch(loginSuccess(data.user));
       } else {
-        // Xử lý khi login không thành công
         setErrorMessage(data.message);
       }
     } catch (error) {
-      // Xử lý lỗi từ server
       console.error("Error:", error.response?.data || error.message);
       alert("An error occurred.");
     }
   };
 
   return (
-    <div className="pt-[200px]">
-      <div className="bg-white shadow-md mx-auto rounded-lg p-6 w-full max-w-sm">
-        <h1 className="text-2xl font-semibold text-center text-gray-800 mb-6">
-          Login
-        </h1>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          {/* Email Input */}
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              placeholder="Enter your email"
-              className="w-full mt-1 px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
-            />
-          </div>
-          {/* Password Input */}
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              placeholder="Enter your password"
-              className="w-full mt-1 px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
-            />
-          </div>
-          <div style={{color: 'red'}}>
-            {errorMessage}
-          </div>
-          {/* Submit Button */}
-          <div>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-red-300 to-blue-300">
+      <div className="flex w-[900px] bg-white shadow-lg rounded-lg overflow-hidden">
+        {/* Left Section */}
+        <div className="w-1/2 bg-gradient-to-r from-red-300 to-blue-300 p-8 flex flex-col justify-center">
+          <h1 className="text-4xl font-bold text-white mb-4">Liver Smart Printing</h1>
+          <p className="text-white text-lg mb-6">Sign in to continue access</p>
+          <div className="text-white mt-auto text-sm">hcmut.edu.vn</div>
+        </div>
+
+        {/* Right Section */}
+        <div className="w-1/2 p-8 flex flex-col justify-center">
+          <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center">Sign In</h2>
+
+          <form onSubmit={handleSubmit} className="space-y-4">
+            {/* Email Input */}
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-600 mb-1"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                placeholder="Enter your email"
+                className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-800"
+              />
+            </div>
+
+            {/* Password Input */}
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-600 mb-1"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                placeholder="Enter your password"
+                className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-800"
+              />
+            </div>
+
+            {errorMessage && (
+              <div className="text-red-500 text-sm">{errorMessage}</div>
+            )}
+
+            {/* Submit Button */}
             <button
               type="submit"
-              className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full bg-gradient-to-r from-red-300 to-blue-300 text-white py-2 px-4 rounded-lg font-bold hover:opacity-90"
             >
-              Login
+              Continue
             </button>
-          </div>
-        </form>
+          </form>
+
+
+        </div>
       </div>
     </div>
   );
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,53 +1,159 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import Cropper from "react-easy-crop";
+import getCroppedImg from "./utils/cropImage"; // Helper function for cropping
 
 const Profile = () => {
     const user = {
         id: "1111111",
-        name: "Mai Đức T",
-        role: "Giảng viên",
-        email: "t.mai@hcmut.edu.vn",
+        name: "Tran Truong Tuan P",
+        role: "Student",
+        email: "pt@hcmut.edu.vn",
         balance: 84,
         lastLogin: "09/12/2023, 23:20:33",
     };
 
+    const [avatar, setAvatar] = useState(null); // Display cropped avatar
+    const [selectedImage, setSelectedImage] = useState(null); // Store the uploaded image
+    const [crop, setCrop] = useState({ x: 0, y: 0 });
+    const [zoom, setZoom] = useState(1);
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+    const [showCropper, setShowCropper] = useState(false);
+
+    const handleAvatarUpload = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                setSelectedImage(reader.result);
+                setShowCropper(true); // Show cropper after selecting image
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+        setCroppedAreaPixels(croppedAreaPixels);
+    }, []);
+
+    const handleCrop = async () => {
+        try {
+            const croppedImage = await getCroppedImg(selectedImage, croppedAreaPixels);
+            setAvatar(croppedImage); // Set the cropped image as avatar
+            setShowCropper(false); // Hide cropper
+        } catch (err) {
+            console.error("Error cropping image:", err);
+        }
+    };
+
+    const handleCancel = () => {
+        setSelectedImage(null);
+        setShowCropper(false); // Close the cropper modal
+    };
+
     return (
-        <div className="h-screen w-full ">
-            <div className="mx-auto mt-8 px-4">
-                <div className="bg-white shadow-lg rounded-lg p-6">
-                    <div className="flex items-center">
-                        {/* Avatar */}
-                        <div className="flex-shrink-0 w-1/2 h-full bg-gray-200 rounded-full flex items-center justify-center">
-                            <span className="text-gray-500 text-6xl">&#128100;</span>
-                        </div>
+        <div className="pt-16 h-full w-full">
+            <div className="w-3/4 h-5/6 mx-auto mt-6 bg-white p-8 rounded-lg shadow-lg">
+                <h1 className="text-center text-3xl font-bold mb-8 text-gray-700">User Profile</h1>
 
-                        {/* User Info */}
-                        <div className="ml-8 flex-grow">
-                            <h1 className="text-2xl font-bold mb-4">Thông tin người dùng</h1>
-                            <div className="grid grid-cols-2 gap-4">
-                                <div className="text-gray-700 font-medium">Mã số sinh viên:</div>
-                                <div className="text-gray-900">{user.id}</div>
-
-                                <div className="text-gray-700 font-medium">Họ và tên:</div>
-                                <div className="text-gray-900">{user.name}</div>
-
-                                <div className="text-gray-700 font-medium">Đối tượng:</div>
-                                <div className="text-gray-900">{user.role}</div>
-
-                                <div className="text-gray-700 font-medium">Email:</div>
-                                <div className="text-gray-900">{user.email}</div>
-
-                                <div className="text-gray-700 font-medium">Số dư (trang A4):</div>
-                                <div className="text-gray-900">{user.balance}</div>
-
-                                <div className="text-gray-700 font-medium">Lần đăng nhập gần nhất:</div>
-                                <div className="text-gray-900">{user.lastLogin}</div>
+                <div className="ml-8 flex-grow">
+                    <div className="flex flex-col items-center mb-8">
+                        <div className="relative">
+                            <div className="w-48 h-48 rounded-full overflow-hidden bg-gray-200 border border-gray-300 flex items-center justify-center mb-3">
+                                {avatar ? (
+                                    <img
+                                        src={avatar}
+                                        alt="User Avatar"
+                                        className="object-cover"
+                                        style={{ width: "100%", height: "100%" }}
+                                    />
+                                ) : (
+                                    <span className="mx-2 text-gray-500 text-xl flex items-center justify-center">
+                                        No Avatar
+                                    </span>
+                                )}
                             </div>
+                            <label
+                                htmlFor="avatarUpload"
+                                className="mx-2 absolute bottom-0 right-2 bg-blue-500 text-white px-3 py-1 rounded-full cursor-pointer text-sm flex items-center justify-center"
+                            >
+                                Upload
+                            </label>
+                            <input
+                                type="file"
+                                id="avatarUpload"
+                                className="hidden"
+                                accept="image/*"
+                                onChange={handleAvatarUpload}
+                            />
+                        </div>
+                    </div>
+
+                    {/* Cropper Modal */}
+                    {showCropper && (
+                        <div className="cropper-modal fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                            <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+                                <h2 className="text-xl font-bold mb-4 text-gray-700 text-center">
+                                    Adjust and Crop Image
+                                </h2>
+                                <div className="cropper-container relative w-full h-64">
+                                    <Cropper
+                                        image={selectedImage}
+                                        crop={crop}
+                                        zoom={zoom}
+                                        aspect={4 / 3} // Aspect ratio
+                                        onCropChange={setCrop}
+                                        onZoomChange={setZoom}
+                                        onCropComplete={onCropComplete}
+                                    />
+                                </div>
+                                <div className="flex justify-end mt-4 gap-4">
+                                    <button
+                                        className="bg-green-500 text-white px-4 py-2 rounded"
+                                        onClick={handleCrop}
+                                    >
+                                        Accept
+                                    </button>
+                                    <button
+                                        className="bg-red-500 text-white px-4 py-2 rounded"
+                                        onClick={handleCancel}
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+
+                    {/* User Details Section */}
+                    <div className="grid grid-cols-2 gap-4 text-3xl">
+                        <div className="border-t border-gray-300 py-3 flex items-center gap-x-1">
+                            <div className="text-gray-700 font-bold">Student ID:</div>
+                            <div className="text-gray-900 ml-auto">{user.id}</div>
+                        </div>
+                        <div className="border-t border-gray-300 py-3 flex">
+                            <div className="text-gray-700 font-bold">Name:</div>
+                            <div className="text-gray-900 ml-auto">{user.name}</div>
+                        </div>
+                        <div className="border-t border-gray-300 py-3 flex">
+                            <div className="text-gray-700 font-bold">Role:</div>
+                            <div className="text-gray-900 ml-auto">{user.role}</div>
+                        </div>
+                        <div className="border-t border-gray-300 py-3 flex">
+                            <div className="text-gray-700 font-bold">Email:</div>
+                            <div className="text-gray-900 ml-auto">{user.email}</div>
+                        </div>
+                        <div className="border-t border-gray-300 py-3 flex">
+                            <div className="text-gray-700 font-bold">Page balance:</div>
+                            <div className="text-gray-900 ml-auto">{user.balance}</div>
+                        </div>
+                        <div className="border-t border-gray-300 py-3 flex">
+                            <div className="text-gray-700 font-bold">Last login:</div>
+                            <div className="text-gray-900 ml-auto">{user.lastLogin}</div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
     );
 };
 
