17702a1bd1a78f2e13bbfaab6de428c8aafebefb
sea835 <hai.ngo1902@hcmut.edu.vn>
2024-12-09 20:05:32 +0700

    modify printing log

--- client/src/components/ReportPage.jsx
+++ client/src/components/ReportPage.jsx
@@ -7,35 +7,38 @@ const ReportPage = () => {
   const [activeTab, setActiveTab] = useState("printing");
 
   return (
-    <div className="pt-16 w-full h-full">
+    <div className="pt-16 w-full h-fit">
       <div className="w-3/4 h-5/6 mx-auto mt-6 bg-white p-8 rounded-lg shadow-lg">
         <h1 className="text-center text-3xl font-bold mb-6 text-gray-800">
           Report Page
         </h1>
         <div className="flex justify-center mb-8">
           <button
-            className={`px-6 py-3 mx-2 transition-colors duration-300 ${activeTab === "printing"
-              ? "bg-blue-600 text-white"
-              : "bg-gray-200 text-gray-800"
-              } rounded-full shadow-md hover:bg-blue-500`}
+            className={`px-6 py-3 mx-2 transition-colors duration-300 ${
+              activeTab === "printing"
+                ? "bg-blue-600 text-white"
+                : "bg-gray-200 text-gray-800"
+            } rounded-full shadow-md hover:bg-blue-500`}
             onClick={() => setActiveTab("printing")}
           >
             Printing Log
           </button>
           <button
-            className={`px-6 py-3 mx-2 transition-colors duration-300 ${activeTab === "payment"
-              ? "bg-blue-600 text-white"
-              : "bg-gray-200 text-gray-800"
-              } rounded-full shadow-md hover:bg-blue-500`}
+            className={`px-6 py-3 mx-2 transition-colors duration-300 ${
+              activeTab === "payment"
+                ? "bg-blue-600 text-white"
+                : "bg-gray-200 text-gray-800"
+            } rounded-full shadow-md hover:bg-blue-500`}
             onClick={() => setActiveTab("payment")}
           >
             Payment Log
           </button>
           <button
-            className={`px-6 py-3 mx-2 transition-colors duration-300 ${activeTab === "account"
-              ? "bg-blue-600 text-white"
-              : "bg-gray-200 text-gray-800"
-              } rounded-full shadow-md hover:bg-blue-500`}
+            className={`px-6 py-3 mx-2 transition-colors duration-300 ${
+              activeTab === "account"
+                ? "bg-blue-600 text-white"
+                : "bg-gray-200 text-gray-800"
+            } rounded-full shadow-md hover:bg-blue-500`}
             onClick={() => setActiveTab("account")}
           >
             Account Management
